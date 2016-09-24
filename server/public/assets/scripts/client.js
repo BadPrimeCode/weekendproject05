3ddf51f5bd7479d5dee64c5bbde45ef0a954b054
Ian <shiny@what.local>
2016-09-24 13:33:58 -0500

    html

---
+++ server/public/assets/scripts/client.js
@@ -0,0 +1,20 @@
+var myApp = angular.module("myApp", ["ngRoute"]);
+
+myApp.config(["$routeProvider", function($routeProvider){
+    $routeProvider.
+      when("/home", {
+        templateUrl: "/views/partials/home.html",
+        controller: "homeController"
+      }).
+      when("/add", {
+        templateUrl: "/views/partials/add.html",
+        controller: "addController"
+      }).
+      when("/pets", {
+        templateUrl: "/views/partials/pets.html",
+        controller: "petsController"
+      }).
+      otherwise({
+        redirectTo: "/home"
+      });
+}]);
