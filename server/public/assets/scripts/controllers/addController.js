eda669d75126b06edc3dcd48616fbf0e2c48546c
Ian <shiny@what.local>
2016-09-25 19:46:59 -0500

    add page

--- server/public/assets/scripts/controllers/addController.js
+++ server/public/assets/scripts/controllers/addController.js
@@ -1,3 +1,9 @@
-myApp.controller("addController", ["$scope", function($scope){
+myApp.controller("addController", ["$scope", "$location", function($scope, $location){
+    $scope.add = function () {
+
+    };
+    $scope.showAll = function() {
+        $location.url('/pets');
+    };
   console.log("in add controller");
 }]);
