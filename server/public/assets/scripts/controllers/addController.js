58f6a885f84fb20a1dd462e4f7ffa16ccdd6b6d7
Ian <shiny@what.local>
2016-09-25 21:04:58 -0500

    controllers

--- server/public/assets/scripts/controllers/addController.js
+++ server/public/assets/scripts/controllers/addController.js
@@ -1,28 +1,14 @@
 myApp.controller("addController", ["$scope", "$location", function($scope, $location){
   console.log("in add controller");
-    $scope.add = function () {
 
-    };
+  $scope.pet = {};
+  $scope.add = function(pet){
+      return $http.post('/add', pet);
+      console.log('post add pet');
+  };
+
     $scope.showAll = function() {
         $location.url('/pets');
     };
 
-}]);
-
-$scope.pet = {};
-$scope.pets = [];
-var getPets = function() {
-    return $http.get('/pets').then(function(response){
-        if(response.status !== 200){
-            throw new Error('Failed to get pets from API');
-        }
-        $scope.pet = {};
-        $scope.pets = response.data;
-        return response.data;
-    })
-};
-$scope.add = function(pet){
-    return $http.post('/add', pet).then(getPets());
-};
-getPets();
 }]);
--- server/public/assets/scripts/controllers/petsController.js
+++ server/public/assets/scripts/controllers/petsController.js
@@ -2,12 +2,13 @@ myApp.controller("petsController", ['$scope', '$http', function($scope, $http){
   console.log("in pets controller");
    $scope.pets = [];
    var getPets = function() {
-       return $http.get('/pets').then(function(response){
-           if(response.status !== 200){
+       return $http.get('/pets').then(function(res){
+           if(res.status !== 200){
                throw new Error('Failed to get pets from API');
            }
-           $scope.pets = response.data;
-           return response.data;
+           $scope.pets = res.data;
+           return res.data;
        })
    };
+   getPets();
 }]);
